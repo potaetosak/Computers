afb9834e1300e29e04c0f98b0f7d8903e078c78a
potaetosak <77297147+potaetosak@users.noreply.github.com>
2021-01-27 15:28:57 -0500

    Add files via upload

---
+++ IntegerArray.h
@@ -0,0 +1,14 @@
+#ifndef __INTEGER_ARRAY__
+#define __INTEGER_ARRAY__
+
+class IntegerArray {
+
+    private:
+        int *data;
+        int size;
+    public:
+        IntegerArray(int size);
+        IntegerArray(IntegerArray &o);
+        ~IntegerArray();
+
+};
